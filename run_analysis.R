67fe0d54e6a5e28e92a00c5d5aec031a8f896e30
beaisis <brianaltman@yahoo.com>
2015-10-23 19:57:25 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -14,43 +14,63 @@
 
 ##########################################################################################################
 
+#load required packages
+if (!require("data.table")) {
+  install.packages("data.table")
+}
+
+if (!require("reshape2")) {
+  install.packages("reshape2")
+}
+
+if (!require("plyr")) {
+  install.packages("plyr")
+}
+
+require("data.table")
+require("reshape2")
+require("plyr")
+
+
 #set working directory for dataset location
-setwd("C:/2015 R Folder/UCI HAR Dataset")
+setwd("C:/2015 R Folder")
 
 #download zip file and unzip files into sub directories
 DataSetUrl      <- "https://https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-DownloadZipData <- "C:/2015 R Folder/UCI HAR Dataset/dataset.zip"
-  
-if(!file.exists(DownloadZipData)) { 
-       download.file(DataSetUrl, DownloadZipData) 
-       unzip(DownloadZipData, exdir = ".")
+
+if(!file.exists("./UCI HAR Dataset/dataset.zip")) { 
+       download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./UCI HAR Dataset/dataset.zip") 
+       unzip("./UCI HAR Dataset/dataset.zip",exdir= ".")
 }
-  
+
+
 #1. Read each set of files and merge into a single file
 
-merge1        <- read.table("C:/2015 R Folder/UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
-merge2        <- read.table("C:/2015 R Folder/UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+merge1        <- read.table("./UCI HAR Dataset/train/X_train.txt")
+merge2        <- read.table("./UCI HAR Dataset/test/X_test.txt")
 mergedX       <- rbind(merge1, merge2)
 rm(merge1, merge2) 
 
-merge1        <- read.table("C:/2015 R Folder/UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt")
-merge2        <- read.table("C:/2015 R Folder/UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt")
+merge1        <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+merge2        <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 mergedY       <- rbind(merge1, merge2)
 rm(merge1, merge2) 
 
-merge1        <- read.table("C:/2015 R Folder/UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
-merge2        <- read.table("C:/2015 R Folder/UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
+merge1        <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+merge2        <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 mergedSubj    <- rbind(merge1, merge2)
 rm(merge1, merge2) 
 
 MergedAll     <- cbind(mergedSubj, mergedY, mergedX)
 rm (mergedSubj, mergedY, mergedX)
 
+
 #2. Replace columns numbers with feature names, subject andactivity
-feature       <- read.table("C:/2015 R Folder/UCI HAR Dataset/UCI HAR Dataset/features.txt")
+feature       <- read.table("./UCI HAR Dataset/features.txt")
 featurename   <- as.character(feature[,2])
 newcolnames   <- c("subject", "activity", featurename)
 colnames(MergedAll) <- newcolnames
+
 
 #3 Select just the mean and standard deviation measures
 MeanAndStdCols   <- grep(".*Mean.*|.*Std.*", colnames(MergedAll), ignore.case=TRUE)
@@ -74,14 +94,17 @@ names(subsetMergedAll)<-gsub("tBody",    "TimeBody",  names(subsetMergedAll))
 
 
 #5 Label the data set with descriptive activity names. 
-activityLabels <- read.table("C:/2015 R Folder/UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt", header = FALSE)
+activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt", header = FALSE)
 subsetMergedAll$activity <- as.character(subsetMergedAll$activity)
 for (i in 1:6){
   subsetMergedAll$activity[subsetMergedAll$activity == i] <- as.character(activityLabels[i,2])
 }
 
 
-#6 Create a tidy data set with average for each variable, activity, subject
+#6 Creates a tidy data set with a average for each variable for each activity and each subject
 tidyData <- aggregate(. ~subject + activity, subsetMergedAll, mean)
 tidyData <- tidyData[order(tidyData$subject,tidyData$activity),]
-write.table(tidyData, file = "Tidy.txt", row.names = FALSE)
+write.csv(tidyData, file = "./UCI HAR Dataset/tidy.csv", row.names = FALSE)
+
+
+  
